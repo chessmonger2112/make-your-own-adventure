eb69ef16f931c35d651fdf321b37a95b2bd949b7
chessmonger2112 <tman2kyle@hotmail.com>
2016-11-12 19:02:55 -0800

    Add summary text to game. Make map object more resemble linked list.

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,66 @@
 $(function(){
 console.log("sup");
-var map = {
-  text: "Welcome to Mongerville!",
+map = {
+  summary: "",
+  prompt: "Which way to go?",
   options: [
     {
       option: "Go east",
-      summary: "You went east.",
-      text: "You have arrived at Magic Mountain",
-      options: [
-        {
-          option: "Go inside",
-          summary: "You went inside. Game Over",
-          text: "You went inside. Game Over",
-          options: []
-        },
-        {
-          option: "Loiter outside like a cool kid.",
-          summary: "You loitered outside so hard, that everyone thinks you're the coolest kid ever. Game Over, you won.",
-          text: "You loitered outside so hard, that everyone thinks you're the coolest kid ever. Game Over, you won.",
-          options: []
-        }
-      ]
+      next: {
+        summary: "You went east.",
+        prompt: "You have arrived at Magic Mountain",
+        options: [
+          {
+            option: "Go inside",
+            next: {
+              summary: "You went inside. Game Over",
+              prompt: "",
+              options: []
+            }
+          },
+          {
+            option: "Loiter outside like a cool kid.",
+            next: {
+              summary: "You loitered outside so hard, that everyone thinks you're the coolest kid ever. Game Over, you won.",
+              prompt: "",
+              options: []
+            }
+          }
+        ]
+      }
     },
     {
       option: "Go west",
-      summary: "You went west.",
-      text: "You have arrived at the ocean",
-      options: [
-        {
-          option: "Go swimming",
-          text: "You went swimming. Game Over",
-          summary: "You went swimming. Game Over",
-          options: []
-        },
-        {
-          option: "Go sunbathing",
-          text: "You went sunbathing. Game Over.",
-          summary: "You went sunbathing. Game Over.",
-          options: []
-        }
-      ]
+      next: {
+        summary: "You went west.",
+        prompt: "You have arrived at the ocean",
+        options: [
+          {
+            option: "Go swimming",
+            next: {
+              summary: "You went swimming. Game Over",
+              prompt: "",
+              options: []
+            }
+          },
+          {
+            option: "Go sunbathing",
+            next: {
+              summary: "You went sunbathing. Game Over.",
+              prompt: "",
+              options: []
+            }
+          }
+        ]
+      }
     }
   ]
 };
 
 var currentNode = map;
-var currentText = currentNode.text;
+var currentPrompt= currentNode.prompt;
 var currentOptions = currentNode.options;
+var currentSummary = currentNode.summary;
 
 function html(element, text, attributes)
 {
@@ -55,21 +69,26 @@ function html(element, text, attributes)
 
 function renderTurn()
 {
-  $("#currentText").text(currentText);
+  $("#currentPrompt").text(currentPrompt);
+  $("#summary").text(currentSummary);
   $("#options").empty();
   currentOptions.forEach((option, index) => $("#options").append(html("li", option.option,"data-index=" + index + " class='option'")));
 
   $(".option").click(function(event) {
     var currentIndex = Number($(this).data("index"));
-    currentNode = currentNode.options[currentIndex];
-    currentText = currentNode.text;
+    currentNode = currentNode.options[currentIndex].next;
+    currentPrompt = currentNode.prompt;
+    currentSummary = currentNode.summary;
     currentOptions = currentNode.options;
 
     renderTurn();
   });
 }
 
+function Scenario()
+{
 
+}
 
 
 
--- scripts/maker.js
+++ scripts/maker.js
@@ -1,19 +1,76 @@
 $(function(){
   console.log("sup!");
+  map = {};
+
+  var currentOptions = ["Option1"];
+  var currentText = "";
+
+
 
 
 
   $("#addOption").click(function() {
-    var newOption = $("#newOption").val();
-    var newElement = html("li", newOption);
-    $(".options").append(newElement);
+    addOption();
+    updateOutput();
+    addToMap();
+  });
 
+  $("#showScenario").click(function() {
+    $("#scenario").show();
   });
 
 
-function html(element, text, attributes="")
+  function addToMap()
+  {
+    map.options = currentOptions;
+    map.prompt = currentText;
+  }
+
+function addOption()
+{
+  var newOption = $("#newOption").val();
+  $("#newOption").val("");
+  var newElement = html("li", newOption);
+  $(".options").append(newElement);
+  currentOptions.push(newOption);
+}
+
+
+function updateOutput()
+{
+  $("#output").empty();
+  updatePrompt();
+  updateOptions();
+}
+
+function updatePrompt()
+{
+  var promptString = "";
+  promptString += "text: "
+  var prompt = $("#prompt").val();
+  promptString += prompt;
+
+  promptString += ",";
+  var newElement = html("div",promptString);
+  currentText = prompt;
+  $("#output").append(newElement);
+}
+
+function updateOptions()
+{
+  var optionsString = "";
+  var output = $("#output");
+  optionsString += "options:[";
+  var elementsString = currentOptions.join(", ");
+  optionsString += elementsString;
+
+  optionsString += "]";
+  output.append(optionsString);
+}
+
+function html(element, text="", attributes="")
 {
   return `<${element} ${attributes}>${text}</${element}>`;
 }
-
+yoh = html;
 });
