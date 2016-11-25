a3450ae84e59e80bbb5ba6f87f8ba512fa6b1bf0
chessmonger2112 <tman2kyle@hotmail.com>
2016-11-24 18:45:32 -0800

    Refactor code, have whole map show up on output window.

--- scripts/maker.js
+++ scripts/maker.js
@@ -1,39 +1,59 @@
 $(function(){
-  console.log("sup!");
-  map = {};
+console.log("sup!");
 
-  var currentOptions = [];
-  var currentText = "";
+map = {}; //by not modifing map explicitly, map will remain the head of the 'linked list'
+currentNode = map;
+currentNode.options = [];
+currentOptions = currentNode.options;
 
+$("#addOption").click(function() {
+  addOptionToView(); //needs to be called before obj is added to the array or index will be off by one.
 
-  $("#addOption").click(function() {
-    addOption();
-    updateOutput();
-    addToMap();
+  addToMap();
+
+  $("#newOption").val("");
+  $("#output").empty(); //has to be before sendMapToOutput call
+
+  sendMapToOutput(map); //displays the information of the map to the output window
+  bindToScenarios(map); // make dictionary of scenarios
+  bindNewScenarios(); //attaching event handler to newly created options
+});
+
+function addToMap()
+{
+  var currentPrompt = $("#prompt").val();
+  var newOption = $("#newOption").val();
+
+  currentNode.prompt = currentPrompt;
+
+  var newOptionObj = {option: newOption, next: null};
+  currentOptions.push(newOptionObj);
+}
+
+function bindNewScenarios()
+{
+  $(".newScenario").unbind().click(function() { //gets called when a new scenario is added
+    var index = $(this).data("number");
+    var newNode = {summary:"", prompt:"", options:[]};
+    currentOptions[index].next = newNode; //attaches the new node to the option
+    currentNode = newNode; //variable currentNode now references the newest node.
+    currentOptions = newNode.options; //current options now references the newest options array
+    currentPrompt = newNode.prompt;
+    clear();
   });
+}
 
-  function bindNewScenarios()
-  {
-    $(".newScenario").unbind().click(function() {
-      var index = $(this).data("number");
-      console.log("new scenario here! " + index);
-    });
-  }
+function clear()
+{
+  $(".options").empty();
+  $("#prompt").val("");
+}
 
-
-  function addToMap()
-  {
-    map.options = currentOptions;
-    map.prompt = currentText;
-  }
-
-function addOption()
+function addOptionToView() //creates and adds the new option to the view
 {
   var newOption = $("#newOption").val();
-  var newOptionElement = html("div", newOption, "class='option optionEditor'");
   var index = currentOptions.length;
-  currentOptions.push(newOption);
-
+  var newOptionElement = html("div", newOption, "class='option optionEditor'");
   var button = html("button", "New","class='newScenario' data-number=" + index);
   var column1 = html("div", newOptionElement, "class='col-md-10 noPadding' ");
   var column2 = html("div", button, "class='col-md-2 noPadding'");
@@ -41,42 +61,41 @@ function addOption()
   var row = html("div", column1 + column2, "class='row'");
   var container = html("div", row, "class='container'");
   var newElement = html("li", row);
-
-  $("#newOption").val("");
   $(".options").append(newElement);
-  bindNewScenarios();
 }
 
-
-function updateOutput()
-{
-  $("#output").empty();
-  updatePrompt();
-  updateOptions();
+function sendMapToOutput(currentNode){
+  var prompt = currentNode.prompt;
+  console.log("prompt: " + prompt);
+  var options = currentNode.options;
+  doUpdateWindow(prompt, options);
+  options.forEach(option => {
+    var next = option.next;
+    if (next)
+    {
+      sendMapToOutput(next)
+    }
+  });
 }
 
-function updatePrompt()
+function doUpdateWindow(prompt, options) //updates the json window from map
 {
-  var promptString = "";
-  promptString += "text: "
-  var prompt = $("#prompt").val();
-  promptString += prompt;
+  doUpdatePrompt(prompt);
+  doUpdateOptions(options);
+}
 
-  promptString += ",";
+function doUpdatePrompt(prompt) //updates the prompt in the json window
+{
+  var promptString = `prompt: ${prompt},`;
   var newElement = html("div", promptString);
-  currentText = prompt;
   $("#output").append(newElement);
 }
 
-function updateOptions()
+function doUpdateOptions(options)
 {
-  var optionsString = "";
   var output = $("#output");
-  optionsString += "options:[";
-  var elementsString = currentOptions.join(", ");
-  optionsString += elementsString;
-
-  optionsString += "]";
+  var elementsString = options.map(option => option.option).join(", ");
+  var optionsString = `options:[${elementsString}]`;
   output.append(optionsString);
 }
 
@@ -84,4 +103,27 @@ function html(element, text="", attributes="")
 {
   return `<${element} ${attributes}>${text}</${element}>`;
 }
+
+scenarios = {};
+
+function bindToScenarios(currentNode) //iterates of map, and adds every scenario to a dictionary
+{
+  var prompt = currentNode.prompt;
+  if (prompt)
+  {
+    scenarios[prompt] = currentNode;
+  }
+  else
+  {
+    var summary = currentNode.summary;
+    scenarios[summary] = currentNode;
+  }
+  currentNode.options.forEach(option => {
+    if (option.next)
+    {
+      bindToScenarios(option.next)
+    }
+  });
+}
+
 });
